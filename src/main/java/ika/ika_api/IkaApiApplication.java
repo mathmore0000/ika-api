e8e10948eb694f44c06c2cd8208abf427b1c8722
Matheus Moreira <matheusmoreira2004@live.com>
2024-08-07 10:51:59 -0300

    chore: changed js -> java

---
+++ src/main/java/ika/ika_api/IkaApiApplication.java
@@ -0,0 +1,13 @@
+package ika.ika_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IkaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IkaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ika/ika_api/IkaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ika.ika_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IkaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
